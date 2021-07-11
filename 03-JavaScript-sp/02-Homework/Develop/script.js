bbcf24d859d5fd7fc9643fa55ffcb73a25e2514a
Sheyla Popovich <Sheylapopovich@gmail.com>
2021-07-11 17:40:20 -0600

    added CharCcode for generator functions

--- 03-JavaScript-sp/02-Homework/Develop/script.js
+++ 03-JavaScript-sp/02-Homework/Develop/script.js
@@ -1,93 +1,200 @@
-// Assignment Code
-
-// when button is clicked generate new password----event 
-// clicked button triggers prompt series for password criteria----method retrieve or update object properties ----->  this performs a task.
-
 //declare variable generateBtn ---> then assign a value
 var generateBtn = document.querySelector("#generate");
-var uppercaseLtr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercaseLtr = "abcdefghijklmnopqrstuvwxyz";
-var numberCharacters = "1234567890";
-var specialCharacters = "!”#$%&’()*+,-./:;=>?@[\]^_`{|}~";
+
+/*var uppercaseLtr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowercaseLtr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numberCharacters = ["1","2","3","4","5","6","7","8","9","0"];
+var specialCharacters = ["!","”","#","$","%","&","’","(",")","*","+",",","-",".","/",":",";","=",">","?","@","[","\","^",","{","|","}","~",";"]
+var passwordLength = "";
+var userInput;
+var numberInput;
+var specialCharInput;*/
 
 
-//create function that will determine the length of the password
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+
+//Generator functions
+function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) +97);
+}
+
+
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) +65);
+}
+
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) +48);
+}
+
+function getRandomSymbol(){
+  const symbols = "!#$%&()*+,-./:;=>?@[\^{|}~";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomLower());
+console.log(getRandomUpper());
+console.log(getRandomNumber());
+console.log(getRandomSymbol());
+
+
+
+
+/*//create function that will determine the length of the password
 function chooseLength(){
-  prompt("Choose password length must be a number between 8 to 128 characters")
+  passwordLength =  prompt("Choose password length must be a number between 8 to 128 characters.");
+  
+  if(passwordLength < 8){
+  alert("Choose a length between 8 and 128 characters long test1");
+  chooseLength();
 
-  if((passwordLength > 8) && (passwordLength < 128)){
-  //do this ---- >
-  alert("Choose length of password between 8 and 128");
-  //  do this ---- >
-  } else(){
+  } else if(passwordLength > 128){
+  alert("Choose a length between 8 and 128 characters long test2");
+  chooseLength();
 
-  }
-  return ;
+  } else{
+    alert("You have chosen your password length please follow next prompts");
+  } 
+  return passwordLength;
 }
 
+  chooseLength(passwordLength); 
 
-//user choose if upperCase or lowerCase ltrs in password
+
+//user choose if upperCase or lowerCase letters in password
 function chooseUpperCaseLtr(){
-  prompt("Do you want uppercase letters in your password? (Type Yes or No)");
+  userInput = prompt("Do you want uppercase letters in your password? (Type Yes or No)");
 
-  if(true === uppercaseLtr){
-    
-  }  else() {
-    alert()
-    chooseUpperCaseLtr();
+  if((userInput === "Yes" || userInput === "yes")){
+    alert ("Your password will consist of uppercase letters. You will now be prompted to choose numeric characters");
+   
+  } else{(userInput === "No" || userInput === "no");
+    alert ("Your password will not consist of uppercase letters. You will now be prompted to choose numeric characters");
   }
-  return ;
-}
-
+    return chooseUpperCaseLtr;
+  }
+ 
+  chooseUpperCaseLtr(userInput);
 
 
 //user choose if numbers will be in password
 function chooseNumbers(){
-  prompt("Do you want numbers in your password?");
+  numberInput = prompt("Do you want numbers in your password? (Type Yes or No)");
 
-  if(){
-
-  } else(){
-
-  }
-  return ;
+  if((numberInput === "Yes" || numberInput === "yes")){
+    alert ("Your password will consist of numeric characters. You will now be prompted to choose special characters");
+        
+  } else{(numberInput === "No" || numberInput === "no");
+    alert ("Your password will not consist of numeric characters. You will now be prompted to choose special characters");
+  }    
+  return chooseNumbers;
 }
+
+chooseNumbers(numberInput);
+
+
 
 
 //user choose (and/or) special characters will be in passwords
 function chooseSpecial(){
-  prompt("Do you want special characters in your password?");
+  specialCharInput = prompt("Do you want special characters in your password?");
 
-  if(){
+  if((specialCharInput === "Yes" || specialCharInput === "yes")){
+    alert ("Your password will consist of special characters.");
+    
+    
+  } else(specialCharInput === "No" || specialCharInput === "no");
+    alert ("Your password will not consist of special characters.");
+     
+  return chooseSpecial;
+}
 
-  } else(){
+chooseSpecial(specialCharInput);*/
 
+function generatePassword(){
+   
+
+  function chooseLength(){
+    passwordLength = prompt("Choose password length must be a number between 8 to 128 characters.");
+    
+    if(passwordLength < 8){
+    alert("Choose a length between 8 and 128 characters long test1");
+    chooseLength();
+  
+    } else if(passwordLength > 128){
+    alert("Choose a length between 8 and 128 characters long test2");
+    chooseLength();
+  
+    } else{
+      alert("You have chosen your password length please follow next prompts");
+    } 
+    return passwordLength;
   }
-  return ;
+  
+    chooseLength(); 
+
+    function chooseUpperCaseLtr(){
+      userInput = prompt("Do you want uppercase letters in your password? (Type Yes or No)");
+    
+      if((userInput === "Yes" || userInput === "yes")){
+        alert ("Your password will consist of uppercase letters. You will now be prompted to choose numeric characters");
+       
+      } else{(userInput === "No" || userInput === "no");
+        alert ("Your password will not consist of uppercase letters. You will now be prompted to choose numeric characters");
+      }
+        return chooseUpperCaseLtr;
+      }
+     
+      chooseUpperCaseLtr(); 
+
+      function chooseNumbers(){
+        numberInput = prompt("Do you want numbers in your password? (Type Yes or No)");
+      
+        if((numberInput === "Yes" || numberInput === "yes")){
+          alert ("Your password will consist of numeric characters. You will now be prompted to choose special characters");
+              
+        } else{(numberInput === "No" || numberInput === "no");
+          alert ("Your password will not consist of numeric characters. You will now be prompted to choose special characters");
+        }    
+        return chooseNumbers;
+      }
+      
+      chooseNumbers();
+
+      function chooseSpecial(){
+        specialCharInput = prompt("Do you want special characters in your password?");
+      
+        if((specialCharInput === "Yes" || specialCharInput === "yes")){
+          alert ("Your password will consist of special characters.");
+          
+          
+        } else(specialCharInput === "No" || specialCharInput === "no");
+          alert ("Your password will not consist of special characters.");
+           
+        return chooseSpecial;
+      }
+       
 }
-
-
-
-
-
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
  
-}
+  generatePassword();
 
+      // Write password to the #password input
+      function writePassword() {
+        var password = generatePassword( uppercaseLtr, lowercaseLtr);
+        var passwordText = document.querySelector("#password");
+        
+        passwordText.value = password;
+      } 
 
+      document.getElementById("password").innerHTML = resultPass
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+      generateBtn.addEventListener("click", writePassword);
 
-//the function is only called on the last line of script
-writePassword();
+      
 
-
-prompt("Enter Password Criteria");
