1382bc983e671330d68c86692c76cb92aaabd904
Sheyla Popovich <Sheylapopovich@gmail.com>
2021-07-12 20:55:19 -0600

    revised code limiting prompt critera

--- 03-JavaScript-sp/02-Homework/Develop/script.js
+++ 03-JavaScript-sp/02-Homework/Develop/script.js
@@ -1,32 +1,19 @@
-//declare variable generateBtn ---> then assign a value
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-/*var uppercaseLtr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowercaseLtr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numberCharacters = ["1","2","3","4","5","6","7","8","9","0"];
-var specialCharacters = ["!","”","#","$","%","&","’","(",")","*","+",",","-",".","/",":",";","=",">","?","@","[","\","^",","{","|","}","~",";"]
-var passwordLength = "";
-var userInput;
+var passwordLength;
+var answerUpper;
 var numberInput;
-var specialCharInput;*/
-
-
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
-};
-
+var specialCharInput;
+let userPick = [];
 
 //Generator functions
-function getRandomLower(){
-  return String.fromCharCode(Math.floor(Math.random() * 26) +97);
-}
-
-
 function getRandomUpper(){
   return String.fromCharCode(Math.floor(Math.random() * 26) +65);
+}
+
+function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) +97);
 }
 
 function getRandomNumber(){
@@ -38,163 +25,122 @@ function getRandomSymbol(){
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
+//Testing random functions
+/*console.log(getRandomUpper());
 console.log(getRandomLower());
-console.log(getRandomUpper());
 console.log(getRandomNumber());
-console.log(getRandomSymbol());
+console.log(getRandomSymbol());*/
 
 
 
 
-/*//create function that will determine the length of the password
-function chooseLength(){
-  passwordLength =  prompt("Choose password length must be a number between 8 to 128 characters.");
-  
-  if(passwordLength < 8){
-  alert("Choose a length between 8 and 128 characters long test1");
-  chooseLength();
-
-  } else if(passwordLength > 128){
-  alert("Choose a length between 8 and 128 characters long test2");
-  chooseLength();
-
-  } else{
-    alert("You have chosen your password length please follow next prompts");
-  } 
-  return passwordLength;
-}
-
-  chooseLength(passwordLength); 
-
-
-//user choose if upperCase or lowerCase letters in password
-function chooseUpperCaseLtr(){
-  userInput = prompt("Do you want uppercase letters in your password? (Type Yes or No)");
-
-  if((userInput === "Yes" || userInput === "yes")){
-    alert ("Your password will consist of uppercase letters. You will now be prompted to choose numeric characters");
-   
-  } else{(userInput === "No" || userInput === "no");
-    alert ("Your password will not consist of uppercase letters. You will now be prompted to choose numeric characters");
-  }
-    return chooseUpperCaseLtr;
-  }
- 
-  chooseUpperCaseLtr(userInput);
-
-
-//user choose if numbers will be in password
-function chooseNumbers(){
-  numberInput = prompt("Do you want numbers in your password? (Type Yes or No)");
-
-  if((numberInput === "Yes" || numberInput === "yes")){
-    alert ("Your password will consist of numeric characters. You will now be prompted to choose special characters");
-        
-  } else{(numberInput === "No" || numberInput === "no");
-    alert ("Your password will not consist of numeric characters. You will now be prompted to choose special characters");
-  }    
-  return chooseNumbers;
-}
-
-chooseNumbers(numberInput);
-
-
-
-
-//user choose (and/or) special characters will be in passwords
-function chooseSpecial(){
-  specialCharInput = prompt("Do you want special characters in your password?");
-
-  if((specialCharInput === "Yes" || specialCharInput === "yes")){
-    alert ("Your password will consist of special characters.");
-    
-    
-  } else(specialCharInput === "No" || specialCharInput === "no");
-    alert ("Your password will not consist of special characters.");
-     
-  return chooseSpecial;
-}
-
-chooseSpecial(specialCharInput);*/
-
+//Prompt options using if and else if statements
 function generatePassword(){
-   
+    function chooseLength(){
+      passwordLength = prompt("Choose password length it must be a number between 8 to 128 characters long.");
+      
+     if(passwordLength < 8){
+      alert("Choose a length between 8 and 128 characters long test1");
+      chooseLength()
 
-  function chooseLength(){
-    passwordLength = prompt("Choose password length must be a number between 8 to 128 characters.");
-    
-    if(passwordLength < 8){
-    alert("Choose a length between 8 and 128 characters long test1");
+      } else if(passwordLength > 128){
+      alert("Choose a length between 8 and 128 characters long test2");
+      chooseLength()
+
+      } else{ alert("You have chosen your password length please follow next prompts");
+      } 
+      return passwordLength;
+    }
+
     chooseLength();
-  
-    } else if(passwordLength > 128){
-    alert("Choose a length between 8 and 128 characters long test2");
-    chooseLength();
-  
-    } else{
-      alert("You have chosen your password length please follow next prompts");
-    } 
-    return passwordLength;
-  }
-  
-    chooseLength(); 
 
     function chooseUpperCaseLtr(){
-      userInput = prompt("Do you want uppercase letters in your password? (Type Yes or No)");
-    
-      if((userInput === "Yes" || userInput === "yes")){
-        alert ("Your password will consist of uppercase letters. You will now be prompted to choose numeric characters");
-       
-      } else{(userInput === "No" || userInput === "no");
-        alert ("Your password will not consist of uppercase letters. You will now be prompted to choose numeric characters");
-      }
-        return chooseUpperCaseLtr;
-      }
-     
-      chooseUpperCaseLtr(); 
+      answerUpper = prompt("Do you want uppercase letters in your password. (Type Yes or No) Then click Ok.");
 
-      function chooseNumbers(){
-        numberInput = prompt("Do you want numbers in your password? (Type Yes or No)");
+      if(answerUpper  === "Yes" || answerUpper === "yes"){
+      alert("You selected Yes. Your password will include uppercase letters");
       
-        if((numberInput === "Yes" || numberInput === "yes")){
-          alert ("Your password will consist of numeric characters. You will now be prompted to choose special characters");
-              
-        } else{(numberInput === "No" || numberInput === "no");
-          alert ("Your password will not consist of numeric characters. You will now be prompted to choose special characters");
-        }    
-        return chooseNumbers;
-      }
-      
-      chooseNumbers();
 
-      function chooseSpecial(){
-        specialCharInput = prompt("Do you want special characters in your password?");
+      } else if(answerUpper  === "No" || answerUpper  === "no"){
+      alert("You selected No. Your password will not include uppercase letters");
       
-        if((specialCharInput === "Yes" || specialCharInput === "yes")){
-          alert ("Your password will consist of special characters.");
-          
-          
-        } else(specialCharInput === "No" || specialCharInput === "no");
-          alert ("Your password will not consist of special characters.");
-           
-        return chooseSpecial;
-      }
-       
-}
- 
-  generatePassword();
 
-      // Write password to the #password input
-      function writePassword() {
-        var password = generatePassword( uppercaseLtr, lowercaseLtr);
-        var passwordText = document.querySelector("#password");
-        
-        passwordText.value = password;
+      } else{ alert("You rebel, you! Please type Yes or No.");
+      chooseUpperCaseLtr()
       } 
+      return chooseUpperCaseLtr;
+    }
 
-      document.getElementById("password").innerHTML = resultPass
+    chooseUpperCaseLtr();
 
-      generateBtn.addEventListener("click", writePassword);
+    function chooseNumbers(){
+      numberInput = prompt("Do you want numbers in your password? (Type Yes or No)");
+    
+      if(numberInput === "Yes" || numberInput === "yes"){
+        alert ("Your password will consist of numeric characters. You will now be prompted to choose special characters");
+            
+      } else if (numberInput === "No" || numberInput === "no"){
+        alert("Your password will not consist of numeric characters. You will now be prompted to choose special characters");
+          
+      } else{ alert ("Mischievous behavior! Please type Yes or No.");
+      chooseNumbers()
+      } 
+      return chooseNumbers;
+    }
+    chooseNumbers();
 
-      
+    const chooseSpecial = () => {
+      specialCharInput = prompt("Do you want special characters in your password?");
+    
+      if(specialCharInput === "Yes" || specialCharInput === "yes"){
+        alert ("Your password will consist of special characters.");
+              
+      } else if (specialCharInput === "No" || specialCharInput === "no"){
+        alert ("Your password will not consist of special characters.");
 
+      } else{ alert ("Mischievous behavior! Please type Yes or No.");
+      chooseSpecial()
+      }    
+      return chooseSpecial;
+    }
+    chooseSpecial();
+}
+
+generatePassword();
+
+
+for(var i=0; i <getRandomUpper.length; i++){
+  userPick.push(getRandomUpper[1]);
+}
+
+for(var i=0; i <getRandomLower.length; i++){
+  userPick.push(getRandomLower[1]);
+}
+
+for(var i=0; i <getRandomNumber.length; i++){
+  userPick.push(getRandomNumber[1]);
+}
+
+for(var i=0; i <getRandomSymbol.length; i++){
+  userPick.push(getRandomSymbol[1]);
+}
+
+var passwordResult = [];
+for(i=0; i < passwordResult; i++){
+  passwordResult.push(userPick[Math.floor(Math.random()* userPick.length)]);
+}
+
+
+
+
+// Write password to the #password input
+  function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- 04-Web-APIs/01-Activities/12-Stu_Event-Listener/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/12-Stu_Event-Listener/Unsolved/assets/js/script.js
@@ -1,3 +1,6 @@
+//It's done when a user clicks on the increment button and the count is increased by 1.
+//It's done when a user clicks on the decrement button and, if the count is greater than 0, the count is decreased by 1.
+
 var count = 0;
 var incrementEl = document.querySelector("#increment");
 var decrementEl = document.querySelector("#decrement");
@@ -8,5 +11,10 @@ function setCounterText() {
 }
 
 // TODO: Add event listener to increment button
+incrementEl.addEventListener('click', function(){
+  setCounterText(-1)
+  
+
+})
 
 // TODO: Add event listener to decrement button 
